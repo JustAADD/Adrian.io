192f532c6c1118d656b7d03639ed609822431d51
adrian makiling <makilingadrian19@gmail.com>
2024-07-11 22:04:38 +0800

    update ko lang ulit

--- content.php
+++ content.php
@@ -8,10 +8,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>ADRIAN.IO</title>
+
+    <link rel="stylesheet" href="/assets/css/cnt.css">
+
+    <!--Bootstrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
 </head>
 
 <body>
+
+    <div class="Atf"> <!--above the footer-->
+
+    </div>
+
+    <div class="footer-end" style=" height: 100px; padding-left: 1% ; padding-right: 1%; padding-top: 1.5%; background-color: #252525; color:#6c6c6c; ">
+
+        <div class="row">
+            <div class="col">
+                © Yea, I also made this website
+            </div>
+            <div class="col">
+                <div class="nav__social" id="footer-social">
+                    <a href="https://www.linkedin.com/in/adrian-makiling-74477928b/" class="nav__social-icon"><ion-icon name="logo-linkedin"></ion-icon></a>
+                    <a href="https://github.com/JustAADD" class="nav__social-icon"><ion-icon name="logo-github"></ion-icon></a>
+                    <a href="mailto:makilingadrian19@gmail.com" class="nav__social-icon"><ion-icon name="mail"></ion-icon></a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
 
