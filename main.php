dfd5aaedc6776c699346d366d667489460506cc5
adrian makiling <makilingadrian19@gmail.com>
2024-07-13 11:24:48 +0800

    update ko lang ulit

--- main.php
+++ main.php
@@ -11,8 +11,105 @@
     <title> ADRIAN.IO</title>
 
     <!-- MAIN CSS--->
-    <link rel="stylesheet" href="assets/css/main.css">
-    
+    <link rel="stylesheet" href="./assets/css/main.css">
+
+    <style>
+        .nav__list {
+            list-style: none;
+            display: flex;
+            gap: 5px;
+            margin-top: 10px;
+            margin-bottom: 10px;
+        }
+
+        .nav__listed {
+            display: flex;
+            justify-content: center;
+
+        }
+
+        .nav__social ion-icon {
+            font-size: 15px;
+            cursor: pointer;
+        }
+
+
+        /*footer*/
+        .abovethefooter {
+            height: 28rem;
+            width: 100%;
+            background-color: #313131;
+
+        }
+
+        .section {
+            width: 100%;
+            height: 0;
+            padding: 10%;
+            margin-left: 5rem;
+        }
+
+        .abovethefooter p {
+            color: #fff;
+            font-family: 'Poppins', sans-serif;
+            font-size: 13px;
+        }
+
+
+
+        .footer {
+            height: 5rem;
+            width: 100%;
+            background-color: #353535;
+            padding: 20px;
+
+        }
+
+        #f-row {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+
+        #f-col {
+            flex: 1;
+            /* Each column takes up equal space */
+            padding: 0 10px;
+            /* Optional: Add padding between columns */
+        }
+
+        .footer p {
+            color: #777777;
+            font-family: 'Poppins', sans-serif;
+            font-size: 13px;
+            margin: 0;
+            text-align: center;
+        }
+
+        .f-nav__social ion-icon {
+            color: #fff;
+        }
+
+        .f-name,
+        .f-email,
+        .f-number {
+            margin-bottom: -5px;
+            /* Adjust spacing between paragraphs */
+        }
+
+        /* Responsive styles (optional) */
+        @media (max-width: 768px) {
+            .row {
+                flex-direction: column;
+                text-align: center;
+            }
+
+            .col {
+                padding: 10px 0;
+                /* Adjust padding for better spacing on small screens */
+            }
+        }
+    </style>
 </head>
 
 <body>
@@ -23,25 +120,27 @@
 
         <nav class="nav" id="nav-menu">
             <div class="nav__content bd-grid">
-
                 <ion-icon name="close-outline" class="nav__close" id="nav-close"></ion-icon>
 
                 <div class="nav__perfil">
                     <div>
-                        <a href="#" class="nav__name"><img src="assets/images/AD.png" alt="" style="height:40px; width: 90px;"></a>
-                        <!-- <span class="nav__profesion">Web designer  </span> -->
+                        <a href="#" class="nav__name">
+                            <img src="assets/images/AD.png" alt="" style="height:40px; width: 90px;">
+                        </a>
                     </div>
                 </div>
 
-                <div class="nav__menu">
+                <div class="nav__listed">
                     <ul class="nav__list">
-
-
-                        <div class="nav__socialidad">
-                            <a href="https://www.linkedin.com/in/adrian-makiling-74477928b/" class="nav__social-icon"><ion-icon name="logo-linkedin"></ion-icon></a>
-                            <a href="https://github.com/JustAADD" class="nav__social-icon"><ion-icon name="logo-github"></ion-icon></a>
-                            <a href="mailto:makilingadrian19@gmail.com" class="nav__social-icon"><ion-icon name="mail"></ion-icon></a>
-                        </div>
+                        <li class="nav__social">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </li>
+                        <li class="nav__social">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </li>
+                        <li class="nav__social">
+                            <ion-icon name="mail-outline"></ion-icon>
+                        </li>
                     </ul>
                 </div>
 
@@ -50,14 +149,44 @@
         </nav>
     </header>
 
-    <!--THE CONTENT-->
 
-    <div class="ediwap">
-        <?php
 
-        include('content.php');
 
-        ?>
+
+    <!--FOOTER-->
+
+    <div class="abovethefooter">
+        <div class="section">
+            <p class="f-contact">Contact</p>
+            <img src="./assets/./images/Adrian M.png" alt="" style="height:4rem; width: 20rem;">
+            <p class="f-name">Adrian Rodrigo Makiling</p>
+            <p class="f-email">makilingadrian19@gmail.com</p>
+            <p class="f-number">(+63) 976-377-0372 </p>
+        </div>
+    </div>
+
+
+    <div class="footer">
+        <div class="row" id="f-row">
+            <div class="col" id="f-col">
+                <p>©Yea, I also made this website</p>
+            </div>
+            <div class="col" id="f-col">
+                <div class="nav__listed">
+                    <ul class="nav__list">
+                        <li class="f-nav__social">
+                            <ion-icon name="logo-linkedin"></ion-icon>
+                        </li>
+                        <li class="f-nav__social">
+                            <ion-icon name="logo-github"></ion-icon>
+                        </li>
+                        <li class="f-nav__social">
+                            <ion-icon name="mail-outline"></ion-icon>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="../assets/js/script.js"></script>
@@ -65,6 +194,9 @@
     <script src="https://unpkg.com/ionicons@5.1.2/dist/ionicons.js"></script>
     <!--===== MAIN JS =====-->
     <script src="assets/js/main.js"></script>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
 </body>
 
