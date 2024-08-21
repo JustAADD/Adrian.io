93644c556edf24c75b5af3c9845c37e708388a62
Aira Esmenda <makilingadrian19@gmail.com>
2024-08-21 10:15:07 +0800

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 /*===== MENU SHOW Y HIDDEN =====*/
-const navMenu = document.getElementById('nav-menu'),
-  toggleMenu = document.getElementById('nav-toggle'),
-  closeMenu = document.getElementById('nav-close')
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('nav-toggle');
+const closeMenu = document.getElementById('nav-close');
 
 /*SHOW*/
 toggleMenu.addEventListener('click', () => {
-  navMenu.classList.toggle('show')
-})
+  navMenu.classList.add('show'); // Add 'show' class to display the menu
+});
 
 /*HIDDEN*/
 closeMenu.addEventListener('click', () => {
-  navMenu.classList.remove('show')
-})
+  navMenu.classList.remove('show'); // Remove 'show' class to hide the menu
+});
 
 /*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');
@@ -22,6 +22,7 @@ function linkAction() {
   this.classList.add('active');
 
   /*Remove menu mobile*/
-  navMenu.classList.remove('show')
+  navMenu.classList.remove('show'); // Hide menu when a link is clicked
 }
+
 navLink.forEach(n => n.addEventListener('click', linkAction));
